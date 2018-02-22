7b5f40b66118eb9322c15d23fad8d5c90e9be943
Katamori <zol.sch93@gmail.com>
2018-02-22 18:13:13 +0100

    Refactor + implementation of generating CSV.

--- src/main/java/com/worldofbuzz/assignment/DatabaseConnector.java
+++ src/main/java/com/worldofbuzz/assignment/DatabaseConnector.java
@@ -3,26 +3,12 @@ package com.worldofbuzz.assignment;
 import java.sql.*;
 import java.util.Properties;
 
-import com.mysql.jdbc.Driver;
-
 public class DatabaseConnector {
 
     public static Properties p;
 
-    public static void getLatest() {
-
-        String getSKU = "SELECT SKU FROM sku_data";
-
-        try {
-
-            Statement stmt = connect().createStatement();
-            ResultSet rs = stmt.executeQuery(getSKU);
-            rs.next();
-            System.out.println("First SKU in the table: "+rs.getString("SKU"));
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static ResultSet query(String string) throws SQLException {
+        return connect().createStatement().executeQuery(string);
     }
 
     // Establish connection to MySQL
--- src/main/java/com/worldofbuzz/assignment/Start.java
+++ src/main/java/com/worldofbuzz/assignment/Start.java
@@ -1,22 +1,67 @@
 package com.worldofbuzz.assignment;
 
+import com.opencsv.CSVWriter;
+
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Start {
     public static void main(String[] args) {
-        Properties props = PropertyLoader.load();
 
-        DatabaseConnector.p = props;
-        DatabaseConnector.getLatest();
+        try(CSVWriter writer = new CSVWriter(
+                Files.newBufferedWriter(Paths.get("./report.csv")),
+                '\t',
+                '"',
+                CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                CSVWriter.DEFAULT_LINE_END)
+        ) {
 
-        try {
-            CSVCreator.main();
-        } catch (IOException e) {
+            String query =
+                "SELECT sku_data.SKU, sku_data.barcode, COUNT(sku_data.SKU)AS amount \n" +
+                    "from \n" +
+                    "(inventory INNER JOIN sku_data ON inventory.SKU = sku_data.SKU)\n" +
+                    "GROUP BY sku_data.SKU";
+
+
+            Properties props = loadProperties();
+            DatabaseConnector.p = props;
+            ResultSet rs = DatabaseConnector.query(query);
+
+
+            //write csv header
+            writer.writeNext(new String[]{"SKU", "Quantity", "SalePrice", "imageURL", "Barcode"});
+
+            //write csv content
+            while(!rs.isLast()){
+                rs.next();
+
+                //SKU lines
+                writer.writeNext(new String[]{
+                        rs.getString("SKU"),
+                        rs.getString("amount"),
+                        "00,00",
+                        "http://testimageUrl.exercise/"+rs.getString("SKU")+".png",
+                        rs.getString("barcode") });
+
+                System.out.println(rs.getString("SKU"));
+            }
+
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    public static Properties loadProperties() throws IOException {
 
+        Properties p = new Properties();
+        p.load(new FileInputStream("general.properties"));
+
+        return p;
     }
 }
 
