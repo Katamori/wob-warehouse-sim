0f0b779437484cdc1f8bdd2350188c723a4554c2
Katamori <zol.sch93@gmail.com>
2018-02-23 15:01:42 +0100

    CSV generator finalized.

--- src/main/java/com/worldofbuzz/assignment/Start.java
+++ src/main/java/com/worldofbuzz/assignment/Start.java
@@ -6,32 +6,47 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Properties;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Start {
+
+    private static Properties props;
+    private static Date today = new Date();
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+
+    static {
+        try {
+            props = loadProperties();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public static void main(String[] args) {
 
-        try(CSVWriter writer = new CSVWriter(
-                Files.newBufferedWriter(Paths.get("./report.csv")),
+        String getSKU =
+            "SELECT sku_data.*, sku_data.barcode, COUNT(sku_data.SKU)AS amount " +
+            "FROM " +
+            "(inventory INNER JOIN sku_data ON inventory.SKU = sku_data.SKU) " +
+            "GROUP BY sku_data.SKU";
+
+        try(
+            CSVWriter writer = new CSVWriter(
+                Files.newBufferedWriter(Paths.get("./report_"+dateFormat.format(today)+".csv")),
                 '\t',
                 '"',
                 CSVWriter.DEFAULT_ESCAPE_CHARACTER,
                 CSVWriter.DEFAULT_LINE_END)
         ) {
 
-            String query =
-                "SELECT sku_data.SKU, sku_data.barcode, COUNT(sku_data.SKU)AS amount \n" +
-                    "from \n" +
-                    "(inventory INNER JOIN sku_data ON inventory.SKU = sku_data.SKU)\n" +
-                    "GROUP BY sku_data.SKU";
-
-
-            Properties props = loadProperties();
-            DatabaseConnector.p = props;
-            ResultSet rs = DatabaseConnector.query(query);
-
+            ResultSet rs = connect().createStatement().executeQuery(getSKU);
 
             //write csv header
             writer.writeNext(new String[]{"SKU", "Quantity", "SalePrice", "imageURL", "Barcode"});
@@ -40,28 +55,84 @@ public class Start {
             while(!rs.isLast()){
                 rs.next();
 
+                String currentSKU = rs.getString("SKU");
+
                 //SKU lines
                 writer.writeNext(new String[]{
-                        rs.getString("SKU"),
-                        rs.getString("amount"),
-                        "00,00",
-                        "http://testimageUrl.exercise/"+rs.getString("SKU")+".png",
-                        rs.getString("barcode") });
-
-                System.out.println(rs.getString("SKU"));
+                    rs.getString("SKU"),
+                    rs.getString("amount"),
+                    calculateSalePrice(currentSKU, rs.getString("RetailPrice")),
+                    "http://testimageUrl.exercise/"+currentSKU+".png",
+                    rs.getString("barcode") });
             }
 
-        } catch (IOException | SQLException e) {
+            System.out.println();
+
+        } catch (IOException | SQLException | ParseException e) {
             e.printStackTrace();
         }
     }
 
-    public static Properties loadProperties() throws IOException {
+
+
+    private static Properties loadProperties() throws IOException {
 
         Properties p = new Properties();
         p.load(new FileInputStream("general.properties"));
 
         return p;
+    }
+
+
+
+    private static Connection connect() throws SQLException {
+
+        return DriverManager.getConnection(
+        "jdbc:mysql://"+props.getProperty("dbaddress")+":3306/"+props.getProperty("database"),
+            props.getProperty("dbuser"),
+            props.getProperty("dbpw"));
+    }
+
+
+
+    private static String calculateSalePrice(String SKU, String price) throws SQLException, ParseException {
+
+        ResultSet dates = connect().createStatement().executeQuery(
+            "SELECT ItemReceivedDate " +
+            "FROM inventory " +
+            "WHERE SKU='"+SKU+"' " +
+            "ORDER BY ItemReceivedDate " +
+            "LIMIT 1"
+        );
+
+        dates.next();
+
+        //get date from DB
+        Date oldest = dateFormat.parse(dates.getString("ItemReceivedDate"));
+
+        // Get diff in msec and "convert" to days
+        long diff = (today.getTime() - oldest.getTime()) / (1000 * 60 * 60 * 24);
+
+        System.out.println(today);
+        System.out.println(oldest);
+        System.out.println(diff);
+
+        double multiplier;
+
+        if(diff < 7){
+            multiplier = 0.65;
+        } else if (diff < 14){
+            multiplier = 0.5;
+        } else if (diff < 28){
+            multiplier = 0.33;
+        } else if (diff < 50){
+            multiplier = 0.25;
+        } else {
+            multiplier = 0.195;
+        }
+
+        return String.valueOf(Double.parseDouble(price)*multiplier);
+
     }
 }
 
